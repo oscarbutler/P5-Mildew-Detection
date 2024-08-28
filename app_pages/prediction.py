dab36fe605dc07557000e2850bf23334478ac6ca
Oscar Butler <oscarbutler48@gmail.com>
2024-08-28 20:23:58 +0000

    Link data frame to image input

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -3,4 +3,7 @@ import streamlit as st
 
 def detector():
 
-    image_input = st.file_uploader('Upload Image of Leaf' type='PNG', accept_multiple_files=True)
+    image_input = st.file_uploader('Upload Image of Leaf', type='PNG', accept_multiple_files=True)
+
+    if image_input is not None:
+        df_report = pd.DataFrame([])
