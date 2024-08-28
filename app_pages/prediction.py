1caef976a1b6f7c977974891aa15f9f049784d9d
Oscar Butler <oscarbutler48@gmail.com>
2024-08-28 19:03:24 +0000

    Use type and accept multiple files

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 def detector():
 
-    image_input = st.file_uploader('Upload Image of Leaf')
+    image_input = st.file_uploader('Upload Image of Leaf' type='PNG', accept_multiple_files=True)
