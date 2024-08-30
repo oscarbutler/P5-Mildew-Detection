89fcaac661320fdce85dc9b0fd18081864269a33
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 13:13:28 +0000

    Give brief information on powdery mildew

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -3,3 +3,6 @@ import streamlit as st
 def summary_function():
 
     st.info("Summary")
+
+    st.info("Powdery mildew is a fungus that can be found in leaves and plants.\n"
+    "It is most commonly find where it is humid and turns part of the leaf or plant into gray or white.")
