a5ced9c5a6a8e618174b9701f3fb4e0a6f5a50ac
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 22:23:51 +0000

    Add model losses and accuracy images

--- app_pages/performance.py
+++ app_pages/performance.py
@@ -11,3 +11,7 @@ def performance_function():
 
     st.image(labels_distribution,
              caption='Labels on Train, Validation and Test Sets')
+    st.info("These two graphs shows the accuracy and losses of the training process.")
+    image_model_loss = plt.imread(f"outputs/{version}/model_losses.png")
+
+    image_model_acc = plt.imread(f"outputs/{version}/model_acc.png")
