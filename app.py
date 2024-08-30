ab20ae3c92700e630ae0705de4ee099ed1c6cc5e
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 16:07:19 +0000

    Add app pages to app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+
+from app_pages.visualiser import visualize_image
+from app_pages.performance import performance_function
+from app_pages.prediction import detector
+from app_pages.summary import summary_function
