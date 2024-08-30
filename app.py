900432c66289e59dfef2aba47351e639ef1c3912
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 16:08:45 +0000

    Add app name and pages

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ from app_pages.visualiser import visualize_image
 from app_pages.performance import performance_function
 from app_pages.prediction import detector
 from app_pages.summary import summary_function
+
+app = MultiPage(app_name="Powdery Mildew Detector")
+
+app.add_page()
+app.add_page()
+app.add_page()
