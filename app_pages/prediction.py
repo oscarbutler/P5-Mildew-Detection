018108e5299bc510c30e00c579363c5b89232246
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 12:47:54 +0000

    Add st.info on if a leaf is healthy or not

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -3,9 +3,9 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+st.info('This page will give us the information on whether we can determine if a leaf is healthy or got powdered mildew.')
+
 def detector():
 
     image_input = st.file_uploader('Upload Image of Leaf', type='PNG', accept_multiple_files=True)
 
-    if image_input is not None:
-        df_report = pd.DataFrame([])
