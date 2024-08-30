152582d26fc3fe4677bf523ea7680ce0929f79d2
Oscar Butler <oscarbutler48@gmail.com>
2024-08-30 22:23:28 +0000

    Add labels distribution image

--- app_pages/performance.py
+++ app_pages/performance.py
@@ -7,3 +7,7 @@ from src.evaluate import load_test_evaluation
 
 def performance_function():
     version = v1
+    labels_distribution = plt.imread(f"outputs/{version}/labels_img_distribution.png")
+
+    st.image(labels_distribution,
+             caption='Labels on Train, Validation and Test Sets')
