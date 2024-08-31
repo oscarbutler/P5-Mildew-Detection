a20ad567f1dde6609777e7e5b5744efff96997ff
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 14:32:47 +0000

    Make dataframe variable

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -7,3 +7,9 @@ from PIL import Image
 from src.data_management import load_pkl_file
 
 def plot_probabilities(probability, prediction_class):
+
+    prob_per_class = pd.DataFrame(
+        data=[0, 0],
+        index={'Parasitised': 0, 'Uninfected': 1}.keys(),
+        columns=['Probability']
+    )
