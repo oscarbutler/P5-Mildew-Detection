2292272789373f0a69e40e2a1d646a5ef07babe0
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 15:50:46 +0000

    Create if statement for images buffer

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -2,10 +2,17 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import pandas as pd
-
-st.info('This page will give us the information on whether we can determine if a leaf is healthy or got powdered mildew.')
+from src.data_management import download_dataframe_as_csv
+from src.machine_learning.predictive_analysis import (
+                                                    load_model_and_predict,
+                                                    resize_input_image,
+                                                    plot_predictions_probabilities
+                                                    )
 
 def detector():
 
     image_input = st.file_uploader('Upload Image of Leaf', type='PNG', accept_multiple_files=True)
 
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
+        for image in images_buffer:
