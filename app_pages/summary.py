1c87f30d430511e46fbaf63ac438ca846172f222
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 22:10:26 +0000

    Add more information about mildew

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -8,6 +8,9 @@ def summary_function():
         f"Powdery mildew is a parasitic fungal disease caused by various species. "
         f"It affects a wide range of plants, "
         f"causing white, powdery spots to appear on the leaves, stems, and sometimes "
-        f"the fruit of the plant. \n\n"
+        f"the fruit of the plant.\n"
         f"As the infection progresses, these spots grow bigger and become so large they combine, "
-        f"eventually covering the entire leaf surface.")
+        f"eventually covering the entire leaf surface."
+        f"Powdery mildew thrives in conditions of high humidity and moderate temperatures, "
+        f"leading to a lot of damage if apporpriatly dealt with if it is not it can lead to lower crop yields."
+    )
