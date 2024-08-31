ee3bbd0051f6f61d3f58062ce116448946b09912
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 21:15:34 +0000

    Add information on powdery mildew

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -4,5 +4,10 @@ def summary_function():
 
     st.info("Summary")
 
-    st.info("Powdery mildew is a fungus that can be found in leaves and plants.\n"
-    "It is most commonly find where it is humid and turns part of the leaf or plant into gray or white.")
+    st.info(
+        f"Powdery mildew is a parasitic fungal disease caused by various species. "
+        f"It affects a wide range of plants, "
+        f"causing white, powdery spots to appear on the leaves, stems, and sometimes "
+        f"the fruit of the plant. \n\n"
+        f"As the infection progresses, these spots grow bigger and become so large they combine, "
+        f"eventually covering the entire leaf surface.")
