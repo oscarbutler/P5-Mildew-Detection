ba648ff690fb24a85964d028ba52223661c26b11
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 16:25:50 +0000

    Fix minor changes

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -6,7 +6,7 @@ from src.data_management import download_dataframe_as_csv
 from src.machine_learning.predictive_analysis import (
                                                     load_model_and_predict,
                                                     resize_input_image,
-                                                    plot_predictions_probabilities
+                                                    plot_probabilities
                                                     )
 
 def detector():
