b9b05115d01a09f8abe8410a60c988a25f698f78
Oscar Butler <oscarbutler48@gmail.com>
2024-08-31 15:14:04 +0000

    Create figure and chart

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -13,10 +13,17 @@ def plot_probabilities(probability, prediction_class):
         index={'Parasitised': 0, 'Uninfected': 1}.keys(),
         columns=['Probability']
     )
-    
+
     prob_per_class.loc[prediction_class] = probability
     for x in prob_per_class.index.to_list():
         if x not in prediction_class:
             prob_per_class.loc[x] = 1 - probability
     prob_per_class = prob_per_class.round(3)
     prob_per_class['Diagnostic'] = prob_per_class.index
+    fig = px.bar(
+        prob_per_class,
+        x='Diagnostic',
+        y=prob_per_class['Probability'],
+        range_y=[0, 1],
+        width=600, height=300, template='seaborn')
+    st.plotly_chart(fig)
