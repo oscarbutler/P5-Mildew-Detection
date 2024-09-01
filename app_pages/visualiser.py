d815ece8de341bb5414142e7fa97d8794057c73e
Oscar Butler <oscarbutler48@gmail.com>
2024-09-01 21:55:06 +0000

    Add captions

--- app_pages/visualiser.py
+++ app_pages/visualiser.py
@@ -17,6 +17,6 @@ def visualize_image():
     average_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
     average_difference = plt.imread(f"outputs/{version}/avg_diff.png")
 
-    st.image(average_parasite, caption="")
-    st.image(average_healthy, caption="")
-    st.image(average_difference, caption="")
+    st.image(average_parasite, caption="Average Of Powdery Mildew Leaves")
+    st.image(average_healthy, caption="Averages Of Healthy Leaves")
+    st.image(average_difference, caption="The average difference")
