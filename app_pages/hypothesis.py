21ff4205ead7de0a7f9406d0b2bca3c680a96dc8
Oscar Butler <oscarbutler48@gmail.com>
2024-09-01 14:10:43 +0000

    Start hypothesis funcrtion

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 def hypothesis_function():
+
+    st.success(
+        f"* Cherry leaves that are affected by powdery mildew will have white spots that appear."
+        f"The model that has been created will be able to determine if a leaf is"
+        f"healthy or afflicted with powdery mildew."
+    )
