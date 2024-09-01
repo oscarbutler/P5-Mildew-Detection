7653e5c48cc1c4a32f97003d5e5180d4863d1cec
Oscar Butler <oscarbutler48@gmail.com>
2024-09-01 13:27:11 +0000

    Add hypothesis page

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ from app_pages.summary import summary_function
 
 app = MultiPage(app_name="Powdery Mildew Detector")
 
+add.app_page("Summary", summary_function)
 app.add_page("Visualiser", visualize_image)
-app.add_page("Performance", performance_function)
-app.add_page("Summary", summary_function)
 app.add_page("Prediction", detector)
+app.add_page("Hypothesis", hypothesis_function)
+app.add_page("Performance", performance_function)
 
 app.run()
