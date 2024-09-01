3298a7452e4486b7318e601ad62945735126ee18
Oscar Butler <oscarbutler48@gmail.com>
2024-09-01 21:29:10 +0000

    Add imread avg vars

--- app_pages/visualiser.py
+++ app_pages/visualiser.py
@@ -13,5 +13,5 @@ def visualize_image():
 
     version = 'v1'
 
-    # average_parasite = plt.imread()
-    # average_healthy = plt.imread()
+    average_parasite = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+    average_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
