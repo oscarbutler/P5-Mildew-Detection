3c9b49d50031ead167be348a0c1894fa975b7b99
Oscar Butler <oscarbutler48@gmail.com>
2024-09-01 00:32:40 +0000

    Change image filter

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -31,11 +31,8 @@ def plot_probabilities(probability, prediction_class):
 def resize_input_image(img, version):
 
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.LANCZOS)
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
     my_image = np.expand_dims(img_resized, axis=0)/255
-
-    if resized_image.mode != 'RGB':
-        resized_image = resized_image.convert('RGB')
 
     return my_image
 
