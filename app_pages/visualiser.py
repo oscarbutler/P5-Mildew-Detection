d92781cec7de6828f8fc05c90168ef835e7be2ec
Oscar Butler <oscarbutler48@gmail.com>
2024-09-02 16:26:21 +0000

    Create image montage function

--- app_pages/visualiser.py
+++ app_pages/visualiser.py
@@ -17,6 +17,10 @@ def visualize_image():
     average_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
     average_difference = plt.imread(f"outputs/{version}/avg_diff.png")
 
+    st.info("This shows the difference between the average powdery mildew and healthy leaves.")
+
     st.image(average_parasite, caption="Average Of Powdery Mildew Leaves")
     st.image(average_healthy, caption="Averages Of Healthy Leaves")
     st.image(average_difference, caption="The average difference")
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
