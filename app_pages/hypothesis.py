42fadc9fb5745a3efad1ca292dc584e4ffede29e
Oscar Butler <oscarbutler48@gmail.com>
2024-09-03 09:22:07 +0000

    Add additional information

--- app_pages/hypothesis.py
+++ app_pages/hypothesis.py
@@ -8,4 +8,12 @@ def hypothesis_function():
         f"* Cherry leaves that are affected by powdery mildew will have white spots that appear."
         f"The model that has been created will be able to determine if a leaf is"
         f"healthy or afflicted with powdery mildew."
+        f"The model that has been created will have a 97% success rate at accuratly identifying"
+        f"powdery mildew in the leaves."
+    )
+
+    st.success(
+        f"Through the user of our visualise page we can determine the"
+        f"biggest differences between a healthy leaf and an"
+        f"infected one."
     )
