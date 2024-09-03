bb8f66ee8f70042887b77cc10aa84cbfc281bccd
Oscar Butler <oscarbutler48@gmail.com>
2024-09-03 03:11:51 +0000

    Change labels

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -6,11 +6,11 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
 
-def plot_probabilities(pred_proba, pred_class):
+def plot_predictions_probabilities(pred_proba, pred_class):
 
     prob_per_class = pd.DataFrame(
         data=[0, 0],
-        index={'healthy': 0, 'powdery_mildew': 1}.keys(),
+        index={'Uninfected': 0, 'Infected': 1}.keys(),
         columns=['Probability']
     )
     prob_per_class.loc[pred_class] = pred_proba
@@ -36,19 +36,20 @@ def resize_input_image(img, version):
 
     return my_image
 
-def model_and_predict(my_image, version):
 
-    model = load_model(f"outputs/{version}/Mildew_Detection_Model.h5")
+def load_model_and_predict(my_image, version):
+
+    model = load_model(f"outputs/{version}/cherry-leaves-model-h1")
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'healthy': 0, 'powdery_mildew': 1}.items()}
+    target_map = {v: k for k, v in {'Uninfected': 0, 'Infected': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample leaf is "
-        f"**{pred_class.lower()}**")
+        f"The predictive analysis indicates the leaf sample is "
+        f"**{pred_class.lower()}** with mildew.")
 
     return pred_proba, pred_class
