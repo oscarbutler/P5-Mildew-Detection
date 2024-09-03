d5b32c99f035d56c050e4f4f0fb012cecb1e10c0
Oscar Butler <oscarbutler48@gmail.com>
2024-09-03 03:18:35 +0000

    Change imports

--- app_pages/prediction.py
+++ app_pages/prediction.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 from src.data_management import download_dataframe_as_csv
 from src.machine_learning.predictive_analysis import (
-                                                    model_and_predict,
+                                                    load_model_and_predict,
                                                     resize_input_image,
-                                                    plot_probabilities
+                                                    plot_predictions_probabilities
                                                     )
 
 def detector():
