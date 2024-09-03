bb48d2e477a5ce3d8523c634cf39dcab2b08aec2
Oscar Butler <oscarbutler48@gmail.com>
2024-09-03 04:45:56 +0000

    Change labels

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -43,7 +43,7 @@ def load_model_and_predict(my_image, version):
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'Uninfected': 0, 'Infected': 1}.items()}
+    target_map = {v: k for k, v in {'Healthy': 0, 'Powdery Mildew': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
