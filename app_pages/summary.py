2f7b16fc8382346e6a6aaa43282dd0270e731466
Oscar Butler <oscarbutler48@gmail.com>
2024-09-03 09:58:10 +0000

    Explain the dataset and the images involved

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -14,3 +14,10 @@ def summary_function():
         f"Powdery mildew thrives in conditions of high humidity and moderate temperatures, "
         f"leading to a lot of damage if apporpriatly dealt with if it is not it can lead to lower crop yields."
     )
+
+    st.success(
+        f"We have recieved the dataset which includes 4208 images"
+        f"and that it is split equally with 2104 being healthy leaves"
+        f"and 2104 being infected leaves."
+        f"This can be found at [Kaggle](https://www.kaggle.com/codeinstitute/cherry-leaves)"
+    )
